05d8e1b7ec6dbf1ae90da224e3c48cf1f150b377
salehkheiri1995-png <salehkheiri1995@gmail.com>
2026-01-29 12:38:16 +0330

    Fix FormPersonnelAnalytics designer - inherit from Form

--- PersonnelManagementApp/FormPersonnelAnalytics.designer.cs
+++ PersonnelManagementApp/FormPersonnelAnalytics.designer.cs
@@ -2,15 +2,8 @@ namespace PersonnelManagementApp
 {
     partial class FormPersonnelAnalytics
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,19 +13,13 @@ namespace PersonnelManagementApp
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormPersonnelAnalytics";
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Name = "FormPersonnelAnalytics";
+            this.Text = "تحلیل کارکنان";
+            this.ResumeLayout(false);
         }
-
-        #endregion
     }
 }
