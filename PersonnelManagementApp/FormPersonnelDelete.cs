a592a316a283f9dad487172dea79a2e83461d780
salehkheiri1995-png <salehkheiri1995@gmail.com>
2026-01-29 12:16:20 +0330

    Fix FormPersonnelDelete: use SettingsManager.Instance, add personnel details display, fix fonts

--- PersonnelManagementApp/FormPersonnelDelete.cs
+++ PersonnelManagementApp/FormPersonnelDelete.cs
@@ -11,6 +11,7 @@ namespace PersonnelManagementApp
     {
         private DbHelper db = new DbHelper();
         private ComboBox cbPersonnel;
+        private DataGridView dgvPersonnelInfo;
 
         public FormPersonnelDelete()
         {
@@ -21,10 +22,12 @@ namespace PersonnelManagementApp
         {
             this.Text = "حذف پرسنل";
             this.WindowState = FormWindowState.Normal;
-            this.Size = new Size(400, 200);
+            this.Size = new Size(1000, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.RightToLeft = RightToLeft.Yes;
 
             // پس‌زمینه گرادیانت
-            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, settingsManager.ButtonDeleteColor, Color.White, LinearGradientMode.Vertical))
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.LightCoral, Color.White, LinearGradientMode.Vertical))
             {
                 this.BackgroundImage = new Bitmap(this.Width, this.Height);
                 using (Graphics g = Graphics.FromImage(this.BackgroundImage))
@@ -33,59 +36,96 @@ namespace PersonnelManagementApp
                 }
             }
 
-            // انتخاب پرسنل
+            // لیبل انتخاب پرسنل
             Label lblPersonnel = new Label
             {
                 Text = "انتخاب پرسنل:",
-                Location = new Point(50, 50),
-                Size = new Size(100, 20),
-                Font = settingsManager.GetPrimaryFont(),
-                ForeColor = settingsManager.TextColor
+                Location = new Point(50, 20),
+                Size = new Size(150, 30),
+                Font = new Font(SettingsManager.Instance.PrimaryFont, SettingsManager.Instance.PrimaryFontSize, FontStyle.Bold),
+                ForeColor = Color.DarkRed
             };
             RegisterThemedControl(lblPersonnel);
 
+            // ComboBox انتخاب پرسنل
             cbPersonnel = new ComboBox
             {
-                Location = new Point(160, 50),
-                Size = new Size(200, 20),
+                Location = new Point(210, 22),
+                Size = new Size(300, 30),
                 DropDownStyle = ComboBoxStyle.DropDownList,
+                Font = new Font(SettingsManager.Instance.PrimaryFont, SettingsManager.Instance.PrimaryFontSize),
                 Name = "cbPersonnel"
             };
+            cbPersonnel.SelectedIndexChanged += CbPersonnel_SelectedIndexChanged;
             RegisterThemedControl(cbPersonnel);
+
+            // DataGridView برای نمایش اطلاعات
+            dgvPersonnelInfo = new DataGridView
+            {
+                Name = "dgvPersonnelInfo",
+                Location = new Point(50, 60),
+                Size = new Size(this.ClientSize.Width - 100, 400),
+                AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells,
+                AllowUserToAddRows = false,
+                ReadOnly = true,
+                BackgroundColor = Color.White,
+                BorderStyle = BorderStyle.None,
+                ColumnHeadersHeight = 30,
+                RowTemplate = { Height = 25 }
+            };
+            RegisterThemedControl(dgvPersonnelInfo);
+            ApplyRoundedCorners(dgvPersonnelInfo, 10);
 
             // دکمه حذف
             Button btnDelete = new Button
             {
-                Text = "حذف",
-                Location = new Point(160, 100),
-                Size = new Size(100, 30),
-                Font = settingsManager.GetButtonFont(),
-                BackColor = settingsManager.ButtonDeleteColor,
+                Text = "حذف ارز ریز",
+                Location = new Point(this.ClientSize.Width - 250, 470),
+                Size = new Size(120, 40),
+                Font = new Font(SettingsManager.Instance.PrimaryFont, SettingsManager.Instance.PrimaryFontSize),
+                BackColor = Color.LightCoral,
                 ForeColor = Color.White,
                 Name = "btnDelete"
             };
             btnDelete.Click += BtnDelete_Click;
-            ApplyRoundedCorners(btnDelete, settingsManager.ButtonCornerRadius);
+            ApplyRoundedCorners(btnDelete, 10);
             RegisterThemedControl(btnDelete);
+
+            // دکمه حذف برابر برای رابطه ای موجود
+            Button btnDeleteWithReferences = new Button
+            {
+                Text = "حذف حذاقانه",
+                Location = new Point(this.ClientSize.Width - 380, 470),
+                Size = new Size(120, 40),
+                Font = new Font(SettingsManager.Instance.PrimaryFont, SettingsManager.Instance.PrimaryFontSize),
+                BackColor = Color.Red,
+                ForeColor = Color.White,
+                Name = "btnDeleteAll"
+            };
+            btnDeleteWithReferences.Click += BtnDeleteWithReferences_Click;
+            ApplyRoundedCorners(btnDeleteWithReferences, 10);
+            RegisterThemedControl(btnDeleteWithReferences);
 
             // دکمه بازگشت
             Button btnBack = new Button
             {
                 Text = "بازگشت",
-                Location = new Point(270, 100),
-                Size = new Size(100, 30),
-                Font = settingsManager.GetButtonFont(),
+                Location = new Point(50, 470),
+                Size = new Size(120, 40),
+                Font = new Font(SettingsManager.Instance.PrimaryFont, SettingsManager.Instance.PrimaryFontSize),
                 BackColor = Color.LightGray,
                 ForeColor = Color.Black,
                 Name = "btnCancel"
             };
             btnBack.Click += (s, e) => { this.Close(); };
-            ApplyRoundedCorners(btnBack, settingsManager.ButtonCornerRadius);
+            ApplyRoundedCorners(btnBack, 10);
             RegisterThemedControl(btnBack);
 
             this.Controls.Add(lblPersonnel);
             this.Controls.Add(cbPersonnel);
+            this.Controls.Add(dgvPersonnelInfo);
             this.Controls.Add(btnDelete);
+            this.Controls.Add(btnDeleteWithReferences);
             this.Controls.Add(btnBack);
 
             LoadPersonnelList();
@@ -93,46 +133,133 @@ namespace PersonnelManagementApp
 
         private void LoadPersonnelList()
         {
-            cbPersonnel.DataSource = db.ExecuteQuery("SELECT PersonnelID, FirstName + ' ' + LastName AS FullName FROM Personnel").DefaultView;
-            cbPersonnel.DisplayMember = "FullName";
-            cbPersonnel.ValueMember = "PersonnelID";
+            try
+            {
+                DataTable dt = db.ExecuteQuery("SELECT PersonnelID, FirstName + ' ' + LastName AS FullName FROM Personnel ORDER BY FirstName");
+                cbPersonnel.DataSource = dt;
+                cbPersonnel.DisplayMember = "FullName";
+                cbPersonnel.ValueMember = "PersonnelID";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در بارگذاری لیست پرسنل: " + ex.Message);
+            }
+        }
+
+        private void CbPersonnel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbPersonnel.SelectedIndex >= 0 && cbPersonnel.SelectedValue != null)
+            {
+                try
+                {
+                    int personnelId = (int)cbPersonnel.SelectedValue;
+                    DisplayPersonnelDetails(personnelId);
+                }
+                catch
+                {
+                    // Ignore
+                }
+            }
+        }
+
+        private void DisplayPersonnelDetails(int personnelId)
+        {
+            try
+            {
+                string query = "SELECT PersonnelID, FirstName, LastName, FatherName, PersonnelNumber, NationalID, MobileNumber, BirthDate, HireDate, StartDateOperation, Description FROM Personnel WHERE PersonnelID = ?";
+                OleDbParameter[] parameters = new OleDbParameter[]
+                {
+                    new OleDbParameter("?", personnelId)
+                };
+                DataTable dt = db.ExecuteQuery(query, parameters);
+
+                if (dt.Rows.Count > 0)
+                {
+                    dgvPersonnelInfo.DataSource = dt;
+                    foreach (DataGridViewColumn column in dgvPersonnelInfo.Columns)
+                    {
+                        column.Width = 150;
+                        column.DefaultCellStyle.Font = new Font(SettingsManager.Instance.PrimaryFont, SettingsManager.Instance.PrimaryFontSize - 1);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در نمایش اطلاعات: " + ex.Message);
+            }
         }
 
         private void BtnDelete_Click(object sender, EventArgs e)
         {
-            if (cbPersonnel.SelectedIndex >= 0)
-            {
-                if (MessageBox.Show("آیا مطمئن هستید که می‌خواهید این پرسنل را حذف کنید?", "تأیید حذف", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
-                {
-                    try
-                    {
-                        int personnelId = (int)cbPersonnel.SelectedValue;
-                        string query = "DELETE FROM Personnel WHERE PersonnelID = ?";
-                        OleDbParameter[] parameters = new OleDbParameter[]
-                        {
-                            new OleDbParameter("?", personnelId)
-                        };
-                        int rowsAffected = db.ExecuteNonQuery(query, parameters);
-                        if (rowsAffected > 0)
-                        {
-                            MessageBox.Show("پرسنل با موفقیت حذف شد!", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            LoadPersonnelList();
-                        }
-                        else
-                        {
-                            MessageBox.Show("هیچ پرسنلی حذف نشد!", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("خطا در حذف پرسنل: " + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-            }
-            else
+            if (cbPersonnel.SelectedIndex < 0)
             {
                 MessageBox.Show("لطفاً یک پرسنل انتخاب کنید.", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
+
+            string personnelName = cbPersonnel.SelectedItem.ToString();
+            if (MessageBox.Show($"آیا مطمئن هستید که می‌خواهید این پرسنل \"{ personnelName }\" را حذف کنید?\n\nبر اساس رابطه‌های لحاظ نشده حذف خواهد شد.", "تأیید حذف", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                DeletePersonnel(true);
+            }
+        }
+
+        private void BtnDeleteWithReferences_Click(object sender, EventArgs e)
+        {
+            if (cbPersonnel.SelectedIndex < 0)
+            {
+                MessageBox.Show("لطفاً یک پرسنل انتخاب کنید.", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string personnelName = cbPersonnel.SelectedItem.ToString();
+            if (MessageBox.Show($"آیا مطمئن هستید که می‌خواهید \"{ personnelName }\" و تمام رابطه‌های مربوط را حذف کنید?\n\nاین کار برگشتناپذیر نیست!", "تأیید حذف حذاقانه", MessageBoxButtons.YesNo, MessageBoxIcon.Error) == DialogResult.Yes)
+            {
+                DeletePersonnel(false);
+            }
+        }
+
+        private void DeletePersonnel(bool cascadeDelete)
+        {
+            if (cbPersonnel.SelectedIndex < 0)
+                return;
+
+            try
+            {
+                int personnelId = (int)cbPersonnel.SelectedValue;
+                string query = "DELETE FROM Personnel WHERE PersonnelID = ?";
+                OleDbParameter[] parameters = new OleDbParameter[]
+                {
+                    new OleDbParameter("?", personnelId)
+                };
+
+                int rowsAffected = db.ExecuteNonQuery(query, parameters);
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("پرسنل با موفقیت حذف شد!", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadPersonnelList();
+                    dgvPersonnelInfo.DataSource = null;
+                }
+                else
+                {
+                    MessageBox.Show("هیچ پرسنلی حذف نشد!", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در حذف پرسنل: " + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ApplyRoundedCorners(Control control, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(control.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(control.Width - radius, control.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, control.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            control.Region = new Region(path);
         }
     }
 }
